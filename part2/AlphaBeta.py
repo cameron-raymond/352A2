8fc1e49d8f24170ac50149248b964934cabec2dc
Cameron Raymond <15cjkr@queensu.ca>
2019-03-12 15:36:50 -0400

    fixed alpha beta output so works if root is min

--- part2/AlphaBeta.py
+++ part2/AlphaBeta.py
@@ -1,32 +1,21 @@
 from Tree import Tree, Node
 import re
 
-# def alpha_beta(root,alpha=-float('inf'),beta=float('inf')):
-#   if root.isLeaf#     return root.data
-#   bestValue = alpha
-#   if root.isMax:
-#     children = root.children
-#     for child in children:
-#       alpha = max(alpha,alpha_beta(child, alpha, beta))
-#       if alpha > beta:
-#         bestValue =
-#     return alpha
-
-
 class AlphaBeta:
-    # print utility value of root node (assuming it is max)
-    # print names of all nodes visited during search
-    numLeavesExamined = 0
-    score = 0
-
+    """
+        Takes in a game tree and performs an alpha beta traversal to find the optimal value given an
+        adversarial opponent. 
+    """
+    
     def __init__(self, game_tree):
         self.game_tree = game_tree  # GameTree
         self.root = game_tree.get_root()  # GameNode
+        self.numLeavesExamined = 0
+        self.score = 0
         return
 
-    def alpha_beta_search(self, node=None):
-        if not node:
-            node = self.root
+    def alpha_beta_search(self):
+        node = self.root
         infinity = float('inf')
         best_val = -infinity
         beta = infinity
@@ -34,19 +23,19 @@ class AlphaBeta:
         successors = self.getSuccessors(node)
         best_state = None
         for state in successors:
-            value = self.min_value(state, best_val, beta)
+            if state.isMax:
+                value = self.max_value(state, best_val, beta)
+            else:
+                value = self.min_value(state, best_val, beta)
             if value > best_val:
                 best_val = value
                 best_state = state
-        print("AlphaBeta:  Utility Value of Root Node: = " + str(best_val))
-        print("AlphaBeta:  Best State is: " + best_state.data)
-        self.score = best_state.data
-        print("{} leaf nodes examined".format(self.numLeavesExamined))
+        self.score = best_val
         return best_state
 
     def max_value(self, node, alpha, beta):
-        print("AlphaBeta-->MAX: Visited Node :: " + node.data)
         if node.isLeaf:
+            self.numLeavesExamined += 1
             return self.getUtility(node)
         infinity = float('inf')
         value = -infinity
@@ -60,7 +49,6 @@ class AlphaBeta:
         return value
 
     def min_value(self, node, alpha, beta):
-        print("AlphaBeta-->MIN: Visited Node :: " + str(node.data))
         if node.isLeaf:
             self.numLeavesExamined += 1
             return self.getUtility(node)
@@ -76,10 +64,6 @@ class AlphaBeta:
 
         return value
 
-    #                     #
-    #   UTILITY METHODS   #
-    #                     #
-
     # successor states in a game tree are the child nodes...
     def getSuccessors(self, node):
         assert node is not None
@@ -88,7 +72,6 @@ class AlphaBeta:
     def getUtility(self, node):
         assert node is not None
         return node.data
-
 
 def read_text(filepath):
     trees = []
@@ -108,7 +91,6 @@ def read_text(filepath):
             trees.append(create_tree(nodes, edges))
     return trees
 
-
 def create_tree(nodes, edges):
     outTree = Tree()
     for node in nodes:
@@ -117,7 +99,6 @@ def create_tree(nodes, edges):
         outTree.add_edge(edge)
     return outTree
 
-
 def format_line(line):
     firstBracket, secondBracket = line.find("{"), line.find("}")
     reduced = line[firstBracket:secondBracket]
@@ -125,17 +106,14 @@ def format_line(line):
     toList = re.findall(regexPat, reduced)
     return toList
 
-
 if __name__ == "__main__":
     trees = read_text('alphaBetaInput.txt')
-    outfile = 'alphaceta_out.txt'
+    outfile = 'alphabeta_out.txt'
 
     with open(outfile, 'w') as f:  # clear text file
         pass
     for i, tree in enumerate(trees):
-        test = AlphaBeta(tree)
-        test.alpha_beta_search()
-        # print(str(tree))
+        alpha_beta = AlphaBeta(tree)
+        alpha_beta.alpha_beta_search()
         with open(outfile, 'a') as f:
-            f.write("Graph: {}, Score: {}, Leaf nodes examined: {}\n".format(
-                i+1, test.score, test.numLeavesExamined))
+            f.write("Graph: {}, Score: {}, Leaf nodes examined: {}\n".format(i+1, alpha_beta.score, alpha_beta.numLeavesExamined))
