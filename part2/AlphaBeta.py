c724705c0457cbe143f06129f4c3d679077dfd2a
Cameron Raymond <15cjkr@queensu.ca>
2019-03-13 12:55:01 -0400

    Added comments, some timers, print statements and changed file name

--- part2/AlphaBeta.py
+++ part2/AlphaBeta.py
@@ -1,5 +1,7 @@
 from Tree import Tree, Node
 import re
+import time 
+
 
 class AlphaBeta:
     """
@@ -7,11 +9,15 @@ class AlphaBeta:
         adversarial opponent. 
     """
     
-    def __init__(self, game_tree):
+    def __init__(self, game_tree,graphID=None):
         self.game_tree = game_tree  # GameTree
         self.root = game_tree.get_root()  # GameNode
         self.numLeavesExamined = 0
         self.score = 0
+        print("Starting Traversal of Graph {}".format(graphID))
+        start = time.time()
+        self.alpha_beta_search()
+        print('Finished - Score:{:d}, Leaf Nodes Examined: {:d}, Total Time: {:f}\n'.format(int(self.score),int(self.numLeavesExamined),time.time()-start))
         return
 
     def alpha_beta_search(self):
@@ -43,7 +49,7 @@ class AlphaBeta:
         successors = self.getSuccessors(node)
         for state in successors:
             value = max(value, self.min_value(state, alpha, beta))
-            if value >= beta:
+            if value >= beta: # Cut off search below current node
                 return value
             alpha = max(alpha, value)
         return value
@@ -58,10 +64,9 @@ class AlphaBeta:
         successors = self.getSuccessors(node)
         for state in successors:
             value = min(value, self.max_value(state, alpha, beta))
-            if value <= alpha:
+            if value <= alpha: #Cut off search below current node
                 return value
             beta = min(beta, value)
-
         return value
 
     # successor states in a game tree are the child nodes...
@@ -113,7 +118,7 @@ if __name__ == "__main__":
     with open(outfile, 'w') as f:  # clear text file
         pass
     for i, tree in enumerate(trees):
-        alpha_beta = AlphaBeta(tree)
-        alpha_beta.alpha_beta_search()
+        alpha_beta = AlphaBeta(tree,i+1)
+        # alpha_beta.alpha_beta_search()
         with open(outfile, 'a') as f:
             f.write("Graph: {}, Score: {}, Leaf nodes examined: {}\n".format(i+1, alpha_beta.score, alpha_beta.numLeavesExamined))
